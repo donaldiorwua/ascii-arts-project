cd99d9cc3b525fdc436555a529f2df4be549cca0
donaldiorwua <donaldiorwua@gmail.com>
2026-05-09 12:48:54 +0100

    Added validation check to splitter function

--- functions/splitter.go
+++ functions/splitter.go
@@ -1,10 +1,14 @@
 package asciiartsproject
 
 import (
+	"fmt"
 	"strings"
 )
 
 func Splitter(data string) []string {
+	if len(data) == 0 {
+		fmt.Println("Error! Banner file is empty!")
+	}
 	lines := strings.Split(data, "\n")
 
 	return lines
