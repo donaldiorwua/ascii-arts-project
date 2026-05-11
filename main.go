82a26abee832e56ab044eab25bae641776009ea1
donaldiorwua <donaldiorwua@gmail.com>
2026-05-11 13:41:15 +0100

    Updated main file

--- main.go
+++ main.go
@@ -2,22 +2,23 @@ package main
 
 import (
 	asciiartsproject "ascii-asrts-project/functions"
+	"fmt"
+	"os"
 )
 
 func main() {
-	asciiartsproject.Improved()
-	// if len(os.Args) < 2 {
-	// 	fmt.Println("Error! Enter a file to read!")
-	// }
+	//asciiartsproject.Improved()
+	if len(os.Args) < 2 {
+		fmt.Println("Error! Enter a file to read!")
+	}
+	inputfile := "standard.txt"
+	inputText := os.Args[1]
 
-	// inputfile := os.Args[1]
-	// inputText := os.Args[2]
+	text := asciiartsproject.FileLoader(inputfile)
+	lines := asciiartsproject.Splitter(text)
+	cMap := asciiartsproject.CharMap(lines)
+	result := asciiartsproject.Renderer(inputText, cMap)
 
-	// text := asciiartsproject.FileLoader(inputfile)
-	// lines := asciiartsproject.Splitter(text)
-	// cMap := asciiartsproject.CharMap(lines)
-	// result := asciiartsproject.Renderer(inputText, cMap)
-
-	// fmt.Println(result)
+	fmt.Print(result)
 
 }
