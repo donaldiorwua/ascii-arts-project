ca1913e48014288ac26b523e4f8de7df362c46da
donaldiorwua <donaldiorwua@gmail.com>
2026-05-11 17:33:14 +0100

    updated splitter function

--- functions/splitter.go
+++ functions/splitter.go
@@ -8,8 +8,12 @@ import (
 func Splitter(data string) []string {
 	if len(data) == 0 {
 		fmt.Println("Error! Banner file is empty!")
+		return nil
 	}
-	lines := strings.Split(strings.TrimRight(data, "\n"), "\n")
+	lines := strings.Split(data, "\n")
+	if lines[0] == "" {
+		lines = lines[1:]
+	}
 
 	return lines
 }
