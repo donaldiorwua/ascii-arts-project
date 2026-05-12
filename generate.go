7d0632cee04365147538fa878ad2a3b556323467
donaldiorwua <donaldiorwua@gmail.com>
2026-05-12 13:50:31 +0100

    ASCII generator function

---
+++ generate.go
@@ -0,0 +1,32 @@
+package main
+
+import "strings"
+
+func GenerateArts(text string, cMap map[rune][]string) string {
+	var result strings.Builder
+	if text == "" {
+		return ""
+	}
+
+	if text == "\\n"{
+		return "\n"
+	}
+
+	words := SplitInput(text)
+
+	for index, lines := range words{
+		if lines == ""{
+			if index != len(words)-1{
+				result.WriteString("\n")
+			}
+			continue
+		}
+		for j := 0; j < 8; j++{
+			for _, char := range lines{
+				result.WriteString(cMap[char][j])
+			}
+			result.WriteString("\n")
+		}
+	}
+	return result.String()
+}
