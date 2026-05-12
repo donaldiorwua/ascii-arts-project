53f99ca56df84158ba95ad00df642d52706aa464
donaldiorwua <donaldiorwua@gmail.com>
2026-05-12 11:49:32 +0100

    Updated validation function

--- validate.go
+++ validate.go
@@ -5,7 +5,7 @@ import "fmt"
 func Validate(input string) (rune, error) {
 	for _, char := range input {
 		if char < ' ' || char > '~' {
-			return char, fmt.Println()("Invalid character")
+			return char, fmt.Errorf("Invalid character: %v", char)
 		}
 	}
 	return 0, nil
