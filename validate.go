3a2826d1d72ddcb79471eb77bf693f285bbed7d7
donaldiorwua <donaldiorwua@gmail.com>
2026-05-13 17:15:48 +0100

    Updated map validation function

--- validate.go
+++ validate.go
@@ -1,12 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Validate(input string) (rune, error) {
-	for _, char := range input {
-		if char < ' ' || char > '~' {
-			return char, fmt.Errorf("Invalid character: %v", char)
+func Validate(input map[rune][]string) (error) {
+	if input == nil{
+		return errors.New("nil map not allowed")
+	}
+	if len(input) == 0 {
+		return errors.New("empty banner file")
+	}
+	if len(input) != 95 {
+		return errors.New("incomplet banner file")
+	}
+
+	for char, block := range input {
+		if char < 32 || char > 126 {
+			return fmt.Errorf("Invalid character: %v", char)
+		}
+		if len(block) !=8 {
+			return errors.New("incomplete block")
+		}
+		for _, line := range block{
+			if line == ""{
+				return errors.New("empty lines not allowed")
+			}
 		}
 	}
-	return 0, nil
+		
+	return nil
 }
